ca1af876c62ffa6d56019ed7e96999d7e4fbccc7
AOURAM <maroua.elmessari3@gmail.com>
2025-07-23 15:54:27 +0800

    Add backend API and setup React client

---
+++ dashboard-client/dashboard-client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MetricsDashboard from './components/MetricsDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FactoryMetrics Dashboard</h1>
+      <MetricsDashboard />
+    </div>
+  );
+}
+
+export default App;
